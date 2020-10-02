4fcfd7706d9fdd4c0f59f06443146ab136ce551f
Adrian Mejia <adrian.mejia.valdez@gmail.com>
2020-10-02 12:00:43 +0200

    adri | update name

---
+++ QuestionApp/__init__.py
@@ -0,0 +1,38 @@
+# Import the framework
+import os
+
+import markdown
+
+from flask import Flask, g
+
+# Create an instance of flask
+from flask_restful import Resource, Api
+
+# Create an instance of Flask
+from test_rest.models.quizzquestion import QuizzQuestion
+
+app = Flask(__name__)
+# Create the API
+api = Api(app)
+
+
+@app.route("/")
+def index():
+    """ Present some documentation """
+
+    # Open the readme file
+    with open(os.path.dirname(app.root_path) + '/README.md', 'r') as markdown_file:
+        # read the content
+        content = markdown_file.read()
+        # Convert to HTML
+        return markdown.markdown(content)
+
+
+class CarlosEsTonto(Resource):
+    def get(self):
+        return "Correcto siempre lo ha sido!"
+
+
+api.add_resource(CarlosEsTonto, '/CarlosEsTonto')
+
+
